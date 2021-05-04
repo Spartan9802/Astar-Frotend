06ebeb3680c8ca05fc10535adcdd1ce32d89cb04
Teddy <metoyerteddy@gmail.com>
2021-05-04 18:29:44 +0200

    create admin page

---
+++ public/admin.php
@@ -0,0 +1,8 @@
+<?php include_once('../head.php') ?>
+    <div id="sidebar" class="column">
+        <h1>test</h1>
+    </div>
+    <div id="right" class="column">
+
+    </div>
+<?php include_once('../footer.php') ?>
