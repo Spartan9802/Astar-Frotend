c91d63654acd1d92a50b860378df5f8c08aff9c5
Teddy <metoyerteddy@gmail.com>
2021-05-05 18:16:46 +0200

    add composer to project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7656250fbf5469b624b99b2f3ed04ac9ba0a2cee',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7656250fbf5469b624b99b2f3ed04ac9ba0a2cee',
+    ),
+    'rbdwllr/reallysimplejwt' => 
+    array (
+      'pretty_version' => '4.0.2',
+      'version' => '4.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a292f6ed1fbecf243dd929245e05784a1228b24c',
+    ),
+  ),
+);
